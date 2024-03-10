77ade1347736fe29d02cb8643089acb751f5ec83
MarkusRenner <markusrenner97@gmx.de>
2024-03-11 00:55:17 +0100

    Created Solution for C# Grasshopper Components with first inputs and comments for further development
    See TTLadapterComponent.cs as main file to work with

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RDF_componenet
+
+Send the RDF data from grasshopper to the web application.
+
+	1. Read graphs from [BHoM] (https://bhom.xyz/documentation/) TTL adapter
+	2. Send this to the web application.
+
+
+## Grasshopper Guides
+Guides to creating custom Grasshopper components and plugins. [RhinoDeveloper] (https://developer.rhino3d.com/guides/grasshopper/)
---
+++ TTLadapter/TTLadapterInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace TTLadapter
+{
+	public class TTLadapterInfo : GH_AssemblyInfo
+	{
+		public override string Name => "TTLadapter";
+
+		//Return a 24x24 pixel bitmap to represent this GHA library.
+		public override Bitmap Icon => null;
+
+		//Return a short string describing the purpose of this GHA library.
+		public override string Description => "";
+
+		public override Guid Id => new Guid("d2410be1-9461-4097-9428-0f29c478bb41");
+
+		//Return a string identifying you or your company.
+		public override string AuthorName => "";
+
+		//Return a string representing your preferred contact details.
+		public override string AuthorContact => "";
+	}
+}
